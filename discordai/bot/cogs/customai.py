bbf8de44fb831f124f6337f26034df4432f05187
Adib <bidabaji@gmail.com>
2023-02-12 08:18:05 -0600

    add openai key to customai

--- discordai/bot/cogs/customai.py
+++ discordai/bot/cogs/customai.py
@@ -28,17 +28,18 @@ class CustomAI(commands.Cog, name="customai"):
         presence_penalty="Number between -2.0 and 2.0. Positive values will encourage new topics: Min=-2 Max=2 Default=0",
         frequency_penalty="Number between -2.0 and 2.0. Positive values will encourage new words: Min=-2 Max=2 Default=0",
         max_tokens="The max number of tokens to generate. Each token costs credits: Default=125",
-        stop="Whether to stop after the first sentence: Default=false")
+        stop="Whether to stop after the first sentence: Default=false",
+        openai_key="The openai key associated with the given model: Default=config.openai_key")
     async def customai(self, context: Context, model: str = "", prompt: str = "", temp: float = 1.0,
                        presence_penalty: float = 0.0, frequency_penalty: float = 0.0, max_tokens: int = 125,
-                       stop: bool = False):
+                       stop: bool = False, openai_key: str = ""):
         temp = min(max(temp, 0), 1)
         presPen = min(max(presence_penalty, -2), 2)
         freqPen = min(max(frequency_penalty, -2), 2)
 
         await context.defer()
         try:
-            openai.api_key = self.bot.config["openai_key"]
+            openai.api_key = openai_key or self.bot.config["openai_key"]
             response = openai.Completion.create(
                 engine=model,
                 prompt=prompt,
