7acc83bbac9094e201f8339a172f9e1177255ca8
Adib <bidabaji@gmail.com>
2023-03-16 16:26:27 -0500

    enum size for imageai command

--- discordai/bot/cogs/imageai.py
+++ discordai/bot/cogs/imageai.py
@@ -9,9 +9,14 @@ Version: 5.4.1
 from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
+from enum import Enum
 
 import openai
 
+class Sizes(Enum):
+    small = "256"
+    medium = "512"
+    large = "1024"
 
 class ImageAI(commands.Cog, name="imageai"):
     def __init__(self, bot):
@@ -23,13 +28,11 @@ class ImageAI(commands.Cog, name="imageai"):
     )
     @app_commands.describe(
         prompt="The prompt to pass to openAI",
-        size="256 | 512 | 1024 pixels:  Default=256")
-    async def openai(self, context: Context, prompt: str , size: str = "256"):
+        size="small: 256px | medium: 512px | large: 1024 px:  Default=small")
+    async def openai(self, context: Context, prompt: str , size: Sizes = Sizes.small):
+        dimension = "256x256" if size.value == "256" else "512x512" if size.value == "512" else "1024x1024"
         await context.defer()
         try:
-            if size not in ["256","512","1024"]:
-                raise Exception("Invalid image size: Must be 256, 512, or 1024")
-            dimension = "256x256" if size == "256" else "512x512" if size == "512" else "1024x1024"
             openai.api_key = self.bot.config["openai_key"]
             response = openai.Image.create(
                 prompt=prompt,
